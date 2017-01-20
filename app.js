d32ea6b29d7d7f9b22879d58b45e50b0e903b0f6
LypkoAlex <lypko.alex@gmail.com>
2017-01-20 13:53:16 +0200

    Added routes from exams

--- app.js
+++ app.js
@@ -51,15 +51,21 @@ router.getAsync(    '/subjects/:subjectId/questions', routes.questions.list.bind
 router.getAsync(    '/specialities/:specialityId/subjects', routes.subjects.list.bind(routes.subjects));
 router.postAsync(   '/specialities/:specialityId/subjects', routes.subjects.create.bind(routes.subjects));
 
-// router.getAsync(    '/subjects/:id', routes.subjects.show.bind(routes.subjects));
-// router.deleteAsync( '/subjects/:id', routes.subjects.delete.bind(routes.subjects));
-// router.patchAsync(  '/subjects/:id', routes.subjects.update.bind(routes.subjects));
 // Speciality
 router.getAsync(    '/specialities',     routes.specialities.list.bind(routes.specialities));
 router.getAsync(    '/specialities/:id', routes.specialities.show.bind(routes.specialities));
 router.postAsync(   '/specialities',     routes.specialities.create.bind(routes.specialities));
 router.deleteAsync( '/specialities/:id', routes.specialities.delete.bind(routes.specialities));
 router.patchAsync(  '/specialities/:id', routes.specialities.update.bind(routes.specialities));
+
+// Exam
+router.getAsync(  '/specialities/:specialityId/exams', routes.exams.list.bind(routes.exams));
+router.postAsync( '/specialities/:specialityId/exams', routes.exams.create.bind(routes.exams));
+
+router.patchAsync(  '/exams/:id', routes.exams.update.bind(routes.exams));
+router.getAsync(    '/exams/:id', routes.exams.show.bind(routes.exams));
+router.deleteAsync( '/exams/:id', routes.exams.delete.bind(routes.exams));
+router.getAsync(    '/exams/',    routes.exams.listAll.bind(routes.exams));
 
 if (!process.env.ENV !== 'test') {
     app.listen(appPort);
